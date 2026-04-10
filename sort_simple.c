aa95ea5933cb52db8fd20ebc0def9a43299d7ba3
Famenontsoa Ny Aina Razanakoto <frazanak@c1r1s4.42antananarivo.mg>
2026-04-10 05:57:52 +0300

    optimized sort_simple

--- sort_simple.c
+++ sort_simple.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_simple.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: srasolov <srasolov@student.42antananari    +#+  +:+       +#+        */
+/*   By: frazanak <frazanak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 10:25:18 by srasolov          #+#    #+#             */
-/*   Updated: 2026/04/09 15:30:05 by srasolov         ###   ########.fr       */
+/*   Updated: 2026/04/10 05:50:22 by frazanak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,16 +63,47 @@ static void	move_min_to_b(t_list **stack_a, t_list **stack_b, t_options *opts)
 	push_b(stack_a, stack_b, opts->bench_mode, &opts->counters);
 }
 
+static void	sort_three(t_list **stack_a, t_options *opts)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = (*stack_a)->value;
+	b = (*stack_a)->next->value;
+	c = (*stack_a)->next->next->value;
+	if (a < b && b < c)
+		return ;
+	else if (a < c && c < b)
+	{
+		swap_a(stack_a, opts->bench_mode, &opts->counters);
+		rotate_a(stack_a, opts->bench_mode, &opts->counters);
+	}
+	else if (b < a && a < c)
+		swap_a(stack_a, opts->bench_mode, &opts->counters);
+	else if (b < c && c < a)
+		rotate_a(stack_a, opts->bench_mode, &opts->counters);
+	else if (c < a && a < b)
+		reverse_a(stack_a, opts->bench_mode, &opts->counters);
+	else if (a < b && b < a)
+	{
+		rotate_a(stack_a, opts->bench_mode, &opts->counters);
+		swap_a(stack_a, opts->bench_mode, &opts->counters);
+	}
+}
+
 void	sort_simple(t_list **stack_a, t_list **stack_, t_options *opts)
 {
 	int	size;
 
 	size = ft_lstsize(*stack_a);
-	while (size > 1)
+	while (size > 3)
 	{
 		move_min_to_b(stack_a, stack_, opts);
 		size--;
 	}
+	if (size == 3)
+		sort_three(stack_a, opts);
 	while (*stack_)
 		push_a(stack_, stack_a, opts->bench_mode, &opts->counters);
 }
