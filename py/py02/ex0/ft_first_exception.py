068e6201e77ae7123f57779ab467b2d5456c43f0
litakel <lucasjusinho@gmail.com>
2026-05-26 16:53:46 +0300

    modif: py02 ec0

--- py/py02/ex0/ft_first_exception.py
+++ py/py02/ex0/ft_first_exception.py
@@ -6,6 +6,7 @@ def test_temperature() -> None:
     print("=== Garden Temperature ===")
 
     tmp_list = ["25", "abc"]
+    not_error = True
 
     for temp_str in tmp_list:
         print()
@@ -15,9 +16,11 @@ def test_temperature() -> None:
             print(f"Temperature is now {result}°C")
         except ValueError as err:
             print(f"Caught input_temperature error: {err}")
+            not_error = False
 
     print()
-    print("All tests completed - program didn't crash")
+    if not_error:
+        print("All tests completed - program didn't crash")
 
 
 def main() -> None:
