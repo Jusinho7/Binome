6aed7ad3f1fcf69216d8899587159980e7a21e89
Sitraka Jose Luc Rasolovoahangy <srasolov@c1r12s14.42antananarivo.mg>
2026-05-27 18:49:16 +0300

    modif: ex0: ft_command_quest.py

--- py/py03/ex0/ft_command_quest.py
+++ py/py03/ex0/ft_command_quest.py
@@ -10,9 +10,8 @@ def main() -> None:
         print("No arguments provided!")
     else:
         print(f"Arguments received: {len(args) - 1}")
-        for i, arg in enumerate(args[1:], start=1):
-            print(f"Argument {i}: {arg}")
-
+        for i in range(1, len(args)):
+            print(f"Argument {i}: {args[i]}")
     print(f"Total arguments: {len(args)}")
 
 
