b232f16c5d3ef35c8a7bc506a589f673cb5f8dbc
Sitraka Jose Luc Rasolovoahangy <srasolov@c1r13s14.42antananarivo.mg>
2026-09-17 21:27:56 +0300

    add new modif on py_cryptic_sorter

--- lv1/py_cryptic_sorter.py
+++ lv1/py_cryptic_sorter.py
@@ -1,5 +1,8 @@
 def cryptic_sorter(strings: list[str]) -> list[str]:
-    return sorted(strings, key=lambda x: (len(x), not x.islower()))
+    return sorted(
+        strings,
+        key=lambda x: (len(x), not x.islower(), sum(i in "aeuioy" for i in x.lower()))
+    )
 
 
 if __name__ == "__main__":
